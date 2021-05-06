3e0aec4ad6a49d8676e3e9fc42aa827c44140920
matkozlowski <mkozlowski@utexas.edu>
2021-05-05 20:40:06 -0500

    Added vision based ball trajectory prediction

--- src/BallSegment.cpp
+++ src/BallSegment.cpp
@@ -10,6 +10,7 @@
 #include <geometry_msgs/Pose.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
+#include <sensor_msgs/Image.h>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
@@ -17,17 +18,72 @@ class BallSegment {
 
 	ros::NodeHandle nh_;
 	image_transport::ImageTransport it_;
-	image_transport::Subscriber image_sub_;
+	//image_transport::Subscriber image_sub_;
 	image_transport::Publisher image_pub_;
-	ros::Subscriber _point_cloud_sub;
+	//ros::Subscriber _point_cloud_sub;
 	cv::Point _ball_center;
 	ros::Publisher _ball_pose_pub;
 
-	public:BallSegment() : it_(nh_) {
-		image_sub_ = it_.subscribe("/camera/color/image_raw", 1, &BallSegment::imageSplit, this);
+	message_filters::Subscriber<sensor_msgs::Image> _im_sub;
+	message_filters::Subscriber<PointCloud> _pc_sub;
+	message_filters::TimeSynchronizer<sensor_msgs::Image, PointCloud> t_sync;
+
+
+
+	public:BallSegment() : it_(nh_), _im_sub(nh_, "/camera/color/image_raw", 1), _pc_sub(nh_, "/camera/depth/points", 1),
+		t_sync(_im_sub, _pc_sub, 10) {
+		//image_sub_ = it_.subscribe("/camera/color/image_raw", 1, &BallSegment::imageSplit, this);
 		image_pub_ = it_.advertise("/color_filter/segmented_ball", 1);
-		_point_cloud_sub = nh_.subscribe<PointCloud>("/camera/depth/points", 1, &BallSegment::sphere_fit, this);
+		//_point_cloud_sub = nh_.subscribe<PointCloud>("/camera/depth/points", 1, &BallSegment::sphere_fit, this);
 		_ball_pose_pub = nh_.advertise<geometry_msgs::Pose>("ball_pose_vision", 1000);
+
+		//t_sync.registerCallback(boost::bind(&syncCB, _1, _2));
+		t_sync.registerCallback(&BallSegment::syncCB, this);
+	}
+
+	void syncCB(const sensor_msgs::ImageConstPtr& img, const PointCloud::ConstPtr& pcloud){
+		//ROS_INFO("BRO");
+
+		cv_bridge::CvImagePtr cv_img;
+		try {
+			cv_img = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+
+		cv::Mat HSVImage;
+		cv::Mat ThreshImage;
+		
+		// transform image into HSV
+		cv::cvtColor(cv_img->image, HSVImage, CV_BGR2HSV);
+		cv::inRange(HSVImage, cv::Scalar(0, 50, 0), cv::Scalar(255, 255, 255), ThreshImage);
+
+		// Get center of the maske out blob using image moments
+		cv::Moments moments = cv::moments(ThreshImage);
+		cv::Point centerPoint(moments.m10/moments.m00, moments.m01/moments.m00);
+		cv::circle(ThreshImage, centerPoint, 2, cv::Scalar(128,0,0), -1);
+
+		//_ball_image = ThreshImage;
+
+		cv_bridge::CvImage segmentedBall(std_msgs::Header(), "mono8", ThreshImage);
+		image_pub_.publish(segmentedBall.toImageMsg());
+
+
+
+		if(centerPoint.x >= 0 && centerPoint.y >= 0){
+			pcl::PointXYZ pc_point = pcloud->at(centerPoint.x, centerPoint.y);
+			printf("-------CENTER-------\n");
+			printf("x: %f, y: %f, z:%f\n", pc_point.x, pc_point.z + 0.25, -pc_point.y + 0.55);
+			
+			geometry_msgs::Pose ball_pose;
+			ball_pose.position.x = pc_point.x;
+			ball_pose.position.y = pc_point.z + 0.25;
+			ball_pose.position.z = -pc_point.y + 0.55;
+			_ball_pose_pub.publish(ball_pose);
+		}
+
+		return;
 	}
 
 	void sphere_fit(const PointCloud::ConstPtr& p_cloud){
